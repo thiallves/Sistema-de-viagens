4247cec9be2b3e56845354407491737fe22cd5f9
Lindsy <lindsy1314@gmail.com>
2026-06-17 10:43:08 -0300

    Update UsuarioRepository.java

--- src/main/java/com/vvv/sistema/repository/UsuarioRepository.java
+++ src/main/java/com/vvv/sistema/repository/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package com.vvv.sistema.repository;
 
-import com.vvv.sistema.model.Usuario;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.vvv.sistema.model.Usuario;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByCpf(String cpf);
 }
